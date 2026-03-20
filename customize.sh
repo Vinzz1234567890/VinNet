07fb83c8cbc70977839d77ef2d09db2e08658211
Vinzz1234567890 <kevinsusanto602@gmail.com>
2026-03-20 10:24:11 +0700

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -1,64 +1,76 @@
 #!/system/bin/sh
-SKIPMOUNT=true
 PROPFILE=false
 POSTFSDATA=false
 LATESTARTSERVICE=true
-
+sleep 2
 ui_print "--------------------------------------------------"
+MetaModules="/data/adb/modules/magic_mount_rs /data/adb/modules/hybrid_mount /data/adb/modules/meta-mm /data/adb/modules/meta-overlayfs /data/adb/modules/magisk_overlayfs /data/adb/modules/mountify"
+MetaModule=false
+for target in $MetaModules; do
+    if [ -d "$target" ]; then
+        ui_print "- Meta Module Detected: $(basename $target)"
+        MetaModule=true
+        break
+    fi
+done
+sleep 1
+if [ "$MetaModule" = true ]; then
+    ui_print "- Using the Meta Module Installation Method"
+    SKIPMOUNT=true
+else
+    ui_print "- Using the KernelSU Installation Method"
+    SKIPMOUNT=false
+fi
+sleep 2
+ui_print "--------------------------------------------------"
+ui_print "- Checking Device Compatibility..."
+sleep 3
 ui_print "- Device Brand: $(getprop ro.product.brand)"
-sleep 0.5
+sleep 1
 ui_print "- Device Model: $(getprop ro.product.model)"
-sleep 0.5
+sleep 1
 ui_print "- OS Version: Android $(getprop ro.build.version.release)"
-sleep 0.5
+sleep 1
 ui_print "- Kernel: $(uname -r)"
-sleep 0.5
+sleep 1
 ui_print "- Architecture: $(getprop ro.product.cpu.abilist)"
 sleep 1
-ui_print "- Checking Device Compatibility..."
-sleep 0.5
 if [ "$(getprop ro.product.device)" == "fog" ]; then
     ui_print "- Device is fog"
 else
     ui_print "- Device isn't fog"
-    sleep 0.5
+    sleep 1
     ui_print "- Delete vendor Configuration"
     rm -rf "$MODPATH/vendor"
 fi
-sleep 1
+sleep 2
+ui_print "--------------------------------------------------"
 ui_print "- Updates:"
-sleep 0.5
+sleep 1
 while read -r line; do
     [ -z "$line" ] && continue
     ui_print "- $line"
-    sleep 0.5
 done <"$MODPATH/updates.txt"
+sleep 2
+ui_print "--------------------------------------------------"
+ui_print "- Notes:"
 sleep 1
-ui_print "- Setting Permissions..."
-sleep 0.5
-ui_print "- Configurating Network..."
-sleep 0.5
-ui_print "- Installing VinNet..."
-sleep 1
-ui_print "- Note: This module is still under development"
-ui_print "        and may cause system instability"
-ui_print "        or bootloop on your device."
-sleep 0.5
-ui_print "        For now, i don't recommend"
-ui_print "        using this module"
-ui_print "        on devices other than the FOG (Redmi 10C),"
-ui_print "        as testing has"
-ui_print "        only been conducted on the FOG."
-sleep 0.5
-ui_print "        I will do their best"
-ui_print "        to improve this module."
-sleep 0.5
-ui_print "        If you encounter any issues"
-ui_print "        while using this module,"
-ui_print "        please report them to developer."
-sleep 1
+ui_print "- This module is still under development and may cause system instability or bootloop on your device."
+ui_print "For now, i don't recommend using this module on devices other than the FOG (Redmi 10C), as testing has only been conducted on the FOG."
+ui_print "I will do their best to improve this module."
+ui_print "If you encounter any issues while using this module, please report them to developer."
+sleep 2
+ui_print "--------------------------------------------------"
 ui_print "- Credit: Vinzz (Developer)"
 ui_print "- TikTok: @vinzz.fog"
 ui_print "- GitHub: @Vinzz1234567890"
+sleep 2
 ui_print "--------------------------------------------------"
-sleep 1
+ui_print "- Setting Permissions..."
+sleep 3
+ui_print "- Configurating Network..."
+sleep 3
+ui_print "- Installing VinNet..."
+sleep 2
+ui_print "--------------------------------------------------"
+am start -a android.intent.action.VIEW -d "https://github.com/Vinzz1234567890/VinNet.git" >/dev/null 2>&1
--- service.sh
+++ service.sh
@@ -7,46 +7,33 @@
 
     sleep 10
 
-    [ -f /proc/sys/net/core/netdev_max_backlog ] && echo "5000" >/proc/sys/net/core/netdev_max_backlog
-    [ -f /proc/sys/net/core/rmem_default ] && echo "262144" >/proc/sys/net/core/rmem_default
-    [ -f /proc/sys/net/core/rmem_max ] && echo "4194304" >/proc/sys/net/core/rmem_max
-    [ -f /proc/sys/net/core/wmem_default ] && echo "262144" >/proc/sys/net/core/wmem_default
-    [ -f /proc/sys/net/core/wmem_max ] && echo "4194304" >/proc/sys/net/core/wmem_max
-    [ -f /proc/sys/net/core/somaxconn ] && echo "4096" >/proc/sys/net/core/somaxconn
-    [ -f /proc/sys/net/core/optmem_max ] && echo "65536" >/proc/sys/net/core/optmem_max
-    [ -f /proc/sys/net/ipv4/tcp_slow_start_after_idle ] && echo "0" >/proc/sys/net/ipv4/tcp_slow_start_after_idle
-    [ -f /proc/sys/net/ipv4/tcp_low_latency ] && echo "1" >/proc/sys/net/ipv4/tcp_low_latency
-    [ -f /proc/sys/net/ipv4/tcp_timestamps ] && echo "1" >/proc/sys/net/ipv4/tcp_timestamps
-    [ -f /proc/sys/net/ipv4/tcp_sack ] && echo "1" >/proc/sys/net/ipv4/tcp_sack
-    [ -f /proc/sys/net/ipv4/tcp_fack ] && echo "1" >/proc/sys/net/ipv4/tcp_fack
-    [ -f /proc/sys/net/ipv4/tcp_window_scaling ] && echo "1" >/proc/sys/net/ipv4/tcp_window_scaling
-    [ -f /proc/sys/net/ipv4/tcp_moderate_rcvbuf ] && echo "1" >/proc/sys/net/ipv4/tcp_moderate_rcvbuf
-    [ -f /proc/sys/net/ipv4/tcp_no_metrics_save ] && echo "1" >/proc/sys/net/ipv4/tcp_no_metrics_save
-    [ -f /proc/sys/net/ipv4/tcp_syn_retries ] && echo "2" >/proc/sys/net/ipv4/tcp_syn_retries
-    [ -f /proc/sys/net/ipv4/tcp_synack_retries ] && echo "2" >/proc/sys/net/ipv4/tcp_synack_retries
-    [ -f /proc/sys/net/ipv4/tcp_retries2 ] && echo "8" >/proc/sys/net/ipv4/tcp_retries2
-    [ -f /proc/sys/net/ipv4/tcp_fin_timeout ] && echo "15" >/proc/sys/net/ipv4/tcp_fin_timeout
-    [ -f /proc/sys/net/ipv4/tcp_rmem ] && echo "4096 87380 4194304" >/proc/sys/net/ipv4/tcp_rmem
-    [ -f /proc/sys/net/ipv4/tcp_wmem ] && echo "4096 87380 4194304" >/proc/sys/net/ipv4/tcp_wmem
-    [ -f /proc/sys/net/ipv4/udp_rmem_min ] && echo "131072" >/proc/sys/net/ipv4/udp_rmem_min
-    [ -f /proc/sys/net/ipv4/udp_wmem_min ] && echo "131072" >/proc/sys/net/ipv4/udp_wmem_min
-    [ -f /proc/sys/net/ipv4/tcp_tw_reuse ] && echo "1" >/proc/sys/net/ipv4/tcp_tw_reuse
-    [ -f /proc/sys/net/ipv4/tcp_max_tw_buckets ] && echo "4096" >/proc/sys/net/ipv4/tcp_max_tw_buckets
-    [ -f /proc/sys/net/ipv4/tcp_max_syn_backlog ] && echo "1024" >/proc/sys/net/ipv4/tcp_max_syn_backlog
-    [ -f /proc/sys/net/ipv4/tcp_fastopen ] && echo "3" >/proc/sys/net/ipv4/tcp_fastopen
-    [ -f /proc/sys/net/ipv4/neigh/default/gc_thresh1 ] && echo "128" >/proc/sys/net/ipv4/neigh/default/gc_thresh1
-    [ -f /proc/sys/net/ipv4/neigh/default/gc_thresh2 ] && echo "1024" >/proc/sys/net/ipv4/neigh/default/gc_thresh2
-    [ -f /proc/sys/net/ipv4/neigh/default/gc_thresh3 ] && echo "2048" >/proc/sys/net/ipv4/neigh/default/gc_thresh3
-    [ -f /proc/sys/net/ipv4/neigh/default/delay_first_probe_time ] && echo "1" >/proc/sys/net/ipv4/neigh/default/delay_first_probe_time
-    [ -f /proc/sys/net/ipv6/neigh/default/delay_first_probe_time ] && echo "1" >/proc/sys/net/ipv6/neigh/default/delay_first_probe_time
-    [ -f /proc/sys/net/ipv4/neigh/default/proxy_delay ] && echo "0" >/proc/sys/net/ipv4/neigh/default/proxy_delay
-    [ -f /proc/sys/net/ipv6/neigh/default/proxy_delay ] && echo "0" >/proc/sys/net/ipv6/neigh/default/proxy_delay
-    [ -f /proc/sys/net/ipv6/conf/all/router_solicitation_delay ] && echo "1" >/proc/sys/net/ipv6/conf/all/router_solicitation_delay
-    [ -f /proc/sys/net/ipv6/conf/default/router_solicitation_delay ] && echo "1" >/proc/sys/net/ipv6/conf/default/router_solicitation_delay
-    [ -f /proc/sys/net/ipv6/conf/wlan0/router_solicitation_delay ] && echo "1" >/proc/sys/net/ipv6/conf/wlan0/router_solicitation_delay
-    [ -f /sys/class/net/wlan0/tx_queue_len ] && echo "1200" >/sys/class/net/wlan0/tx_queue_len
+    [ -f /proc/sys/net/core/netdev_max_backlog ] && echo "1000" >/proc/sys/net/core/netdev_max_backlog                                  # Default netdev_max_backlog=1000
+    [ -f /proc/sys/net/core/rmem_default ] && echo "212992" >/proc/sys/net/core/rmem_default                                            # Default rmem_default=212992
+    [ -f /proc/sys/net/core/rmem_max ] && echo "16777216" >/proc/sys/net/core/rmem_max                                                  # Default rmem_max=16777216
+    [ -f /proc/sys/net/core/wmem_default ] && echo "212992" >/proc/sys/net/core/wmem_default                                            # Default wmem_default=212992
+    [ -f /proc/sys/net/core/wmem_max ] && echo "8388608" >/proc/sys/net/core/wmem_max                                                   # Default wmem_max=8388608
+    [ -f /proc/sys/net/core/somaxconn ] && echo "128" >/proc/sys/net/core/somaxconn                                                     # Default somaxconn=128
+    [ -f /proc/sys/net/ipv4/tcp_slow_start_after_idle ] && echo "0" >/proc/sys/net/ipv4/tcp_slow_start_after_idle                       # Default tcp_slow_start_after_idle=1
+    [ -f /proc/sys/net/ipv4/tcp_low_latency ] && echo "1" >/proc/sys/net/ipv4/tcp_low_latency                                           # Default tcp_low_latency=0
+    [ -f /proc/sys/net/ipv4/tcp_sack ] && echo "1" >/proc/sys/net/ipv4/tcp_sack                                                         # Default tcp_sack=1
+    [ -f /proc/sys/net/ipv4/tcp_fack ] && echo "1" >/proc/sys/net/ipv4/tcp_fack                                                         # Default tcp_fack=0
+    [ -f /proc/sys/net/ipv4/tcp_window_scaling ] && echo "1" >/proc/sys/net/ipv4/tcp_window_scaling                                     # Default tcp_window_scaling=1
+    [ -f /proc/sys/net/ipv4/tcp_moderate_rcvbuf ] && echo "1" >/proc/sys/net/ipv4/tcp_moderate_rcvbuf                                   # Default tcp_moderate_rcvbuf=1
+    [ -f /proc/sys/net/ipv4/tcp_no_metrics_save ] && echo "1" >/proc/sys/net/ipv4/tcp_no_metrics_save                                   # Default tcp_no_metrics_save=0
+    [ -f /proc/sys/net/ipv4/tcp_syn_retries ] && echo "4" >/proc/sys/net/ipv4/tcp_syn_retries                                           # Default tcp_syn_retries=4
+    [ -f /proc/sys/net/ipv4/tcp_synack_retries ] && echo "3" >/proc/sys/net/ipv4/tcp_synack_retries                                     # Default tcp_synack_retries=3
+    [ -f /proc/sys/net/ipv4/tcp_retries2 ] && echo "8" >/proc/sys/net/ipv4/tcp_retries2                                                 # Default tcp_retries2=8
+    [ -f /proc/sys/net/ipv4/tcp_rmem ] && echo "524288 212992 16777216" >/proc/sys/net/ipv4/tcp_rmem                                    # Default tcp_rmem=524288 1048576 2097152
+    [ -f /proc/sys/net/ipv4/tcp_wmem ] && echo "262144 212992 8388608" >/proc/sys/net/ipv4/tcp_wmem                                     # Default tcp_wmem=262144 212992 8388608
+    [ -f /proc/sys/net/ipv4/udp_rmem_min ] && echo "5000" >/proc/sys/net/ipv4/udp_rmem_min                                              # Default udp_rmem_min=4096
+    [ -f /proc/sys/net/ipv4/udp_wmem_min ] && echo "5000" >/proc/sys/net/ipv4/udp_wmem_min                                              # Default udp_wmem_min=4096
+    [ -f /proc/sys/net/ipv4/tcp_tw_reuse ] && echo "2" >/proc/sys/net/ipv4/tcp_tw_reuse                                                 # Default tcp_tw_reuse=2
+    [ -f /proc/sys/net/ipv4/tcp_max_syn_backlog ] && echo "200" >/proc/sys/net/ipv4/tcp_max_syn_backlog                                 # Default tcp_max_syn_backlog=128
+    [ -f /proc/sys/net/ipv4/tcp_fastopen ] && echo "3" >/proc/sys/net/ipv4/tcp_fastopen                                                 # Default tcp_fastopen=1
+    [ -f /proc/sys/net/ipv4/neigh/default/delay_first_probe_time ] && echo "5" >/proc/sys/net/ipv4/neigh/default/delay_first_probe_time # Default delay_first_probe_time=5
+    [ -f /sys/class/net/wlan0/tx_queue_len ] && echo "2000" >/sys/class/net/wlan0/tx_queue_len                                          # Default tx_queue_len=3000
 
-    [ -f /proc/sys/net/ipv4/tcp_congestion_control ] && echo "westwood" >/proc/sys/net/ipv4/tcp_congestion_control
+    [ -f /proc/sys/net/ipv4/tcp_congestion_control ] && echo "bbrplus" >/proc/sys/net/ipv4/tcp_congestion_control
     [ -f /sys/class/net/wlan0/queues/rx-0/rps_cpus ] && echo "ff" >/sys/class/net/wlan0/queues/rx-0/rps_cpus
     iptables -t mangle -A OUTPUT -p udp --dport 5000:6000 -j TOS --set-tos 0x10
     iptables -t mangle -A OUTPUT -p udp --dport 3000:4000 -j TOS --set-tos 0x10
@@ -57,6 +44,7 @@
     iptables -t mangle -A OUTPUT -p tcp --tcp-flags FIN,SYN,RST,ACK ACK -m length --length 40:100 -j TOS --set-tos 0x10
     [ -f /proc/sys/net/core/busy_poll ] && echo "50" >/proc/sys/net/core/busy_poll
     [ -f /proc/sys/net/core/busy_read ] && echo "50" >/proc/sys/net/core/busy_read
+    [ -f /proc/sys/net/core/default_qdisc ] && echo "cake" >/proc/sys/net/core/default_qdisc
 
     # Bonus
     setprop debug.sf.enable_adpf_cpu_hint 1
