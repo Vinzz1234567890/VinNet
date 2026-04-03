1a4d9cd4816c5632a45d0b475e2a4223b5b3f340
Vinzz1234567890 <kevinsusanto602@gmail.com>
2026-04-03 19:51:38 +0700

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 LATESTARTSERVICE=true
-sleep 2
-ui_print "--------------------------------------------------"
+sleep 5
+ui_print "──────────────────────────────────────────────"
 MetaModules="/data/adb/modules/magic_mount_rs /data/adb/modules/hybrid_mount /data/adb/modules/meta-mm /data/adb/modules/meta-overlayfs /data/adb/modules/magisk_overlayfs /data/adb/modules/mountify"
 MetaModule=false
 for target in $MetaModules; do
@@ -11,66 +11,69 @@ for target in $MetaModules; do
         break
     fi
 done
-sleep 1
+sleep 0.2
 if [ "$MetaModule" = true ]; then
     ui_print "- Using $(basename $target) Mounting Method"
     SKIPMOUNT=true
 else
     ui_print "- Using Standard Mounting Method"
-    ui_print "- Using standard mounting method results in a failure rate of approximately 99% on latest Android systems"
-    ui_print "- I recommend installing meta module before installing this module"
     SKIPMOUNT=false
 fi
-sleep 2
-ui_print "--------------------------------------------------"
+sleep 0.5
+ui_print "──────────────────────────────────────────────"
 ui_print "- Checking Device Compatibility..."
-sleep 3
+sleep 0.2
 ui_print "- Device Brand: $(getprop ro.product.brand)"
-sleep 1
 ui_print "- Device Model: $(getprop ro.product.model)"
-sleep 1
 ui_print "- OS Version: Android $(getprop ro.build.version.release)"
-sleep 1
 ui_print "- Kernel: $(uname -r)"
-sleep 1
 ui_print "- Architecture: $(getprop ro.product.cpu.abilist)"
-sleep 1
+
 if [ "$(getprop ro.product.device)" = "fog" ]; then
     ui_print "- Device is fog"
 else
     ui_print "- Device isn't fog"
-    sleep 1
+    sleep 0.2
     ui_print "- Delete vendor Configuration"
-    rm -rf "$MODPATH/vendor"
+    rm -rf "$MODPATH/system/vendor"
 fi
-sleep 2
-ui_print "--------------------------------------------------"
-ui_print "- Updates:"
-sleep 1
-while read -r line; do
-    [ -z "$line" ] && continue
-    ui_print "$line"
-done <"$MODPATH/updates.txt"
-sleep 2
-ui_print "--------------------------------------------------"
-ui_print "- Notes:"
-sleep 1
-ui_print "- This module is still under development and may cause system instability or bootloop on your device."
-ui_print "For now, i don't recommend using this module on devices other than the FOG (Redmi 10C), as testing has only been conducted on the FOG."
-ui_print "I will do their best to improve this module."
-ui_print "If you encounter any issues while using this module, please report them to developer."
-sleep 2
-ui_print "--------------------------------------------------"
-ui_print "- Credit: Vinzz (Developer)"
+sleep 0.5
+ui_print "──────────────────────────────────────────────"
+ui_print "Checking Binary Dependencies..."
+sleep 0.2
+Binaries="/system/bin/iw* /vendor/bin/iw* /system/xbin/iw*"
+Binary=false
+for target in $Binaries; do
+    if [ -f "$target" ]; then
+        ui_print "- Built-in Binary Detected: $(basename $target)"
+        Binary=true
+        break
+    fi
+done
+sleep 0.2
+if [ "$Binary" = true ]; then
+    ui_print "- Using Built-in Binary: $(basename $target)"
+else
+    ui_print "- Built-in Binary not Detected, Installing binary..."
+    case $ARCH in
+    arm64) cp -f "$MODPATH/binaries/iw-arm64" "$MODPATH/system/bin/iw" ;;
+    arm) cp -f "$MODPATH/binaries/iw-arm" "$MODPATH/system/bin/iw" ;;
+    *) abort "Architecture not Supported" ;;
+    esac
+fi
+sleep 0.5
+ui_print "──────────────────────────────────────────────"
+ui_print "- Credit: Vinzz"
 ui_print "- TikTok: @vinzz.fog"
 ui_print "- GitHub: @Vinzz1234567890"
-sleep 2
-ui_print "--------------------------------------------------"
+sleep 0.5
+ui_print "──────────────────────────────────────────────"
 ui_print "- Setting Permissions..."
-sleep 3
+set_perm "$MODPATH/system/bin/iw" 0 0 0755
+sleep 0.2
 ui_print "- Configurating Network..."
-sleep 3
+sleep 0.2
 ui_print "- Installing VinNet..."
-sleep 2
-ui_print "--------------------------------------------------"
+sleep 0.5
+ui_print "──────────────────────────────────────────────"
 am start -a android.intent.action.VIEW -d "https://github.com/Vinzz1234567890/VinNet.git" >/dev/null 2>&1
--- service.sh
+++ service.sh
@@ -4,33 +4,37 @@
         sleep 5
     done
     sleep 10
-    [ -f /proc/sys/net/core/netdev_max_backlog ] && echo "300" >/proc/sys/net/core/netdev_max_backlog                                   # Default netdev_max_backlog=1000
-    [ -f /proc/sys/net/core/rmem_default ] && echo "262144" >/proc/sys/net/core/rmem_default                                            # Default rmem_default=212992
-    [ -f /proc/sys/net/core/rmem_max ] && echo "4194304" >/proc/sys/net/core/rmem_max                                                   # Default rmem_max=16777216
-    [ -f /proc/sys/net/core/wmem_default ] && echo "262144" >/proc/sys/net/core/wmem_default                                            # Default wmem_default=212992
-    [ -f /proc/sys/net/core/wmem_max ] && echo "4194304" >/proc/sys/net/core/wmem_max                                                   # Default wmem_max=8388608
-    [ -f /proc/sys/net/core/somaxconn ] && echo "1024" >/proc/sys/net/core/somaxconn                                                    # Default somaxconn=128
-    [ -f /proc/sys/net/ipv4/tcp_slow_start_after_idle ] && echo "0" >/proc/sys/net/ipv4/tcp_slow_start_after_idle                       # Default tcp_slow_start_after_idle=1
-    [ -f /proc/sys/net/ipv4/tcp_low_latency ] && echo "1" >/proc/sys/net/ipv4/tcp_low_latency                                           # Default tcp_low_latency=0
-    [ -f /proc/sys/net/ipv4/tcp_sack ] && echo "1" >/proc/sys/net/ipv4/tcp_sack                                                         # Default tcp_sack=1
-    [ -f /proc/sys/net/ipv4/tcp_fack ] && echo "1" >/proc/sys/net/ipv4/tcp_fack                                                         # Default tcp_fack=0
-    [ -f /proc/sys/net/ipv4/tcp_window_scaling ] && echo "1" >/proc/sys/net/ipv4/tcp_window_scaling                                     # Default tcp_window_scaling=1
-    [ -f /proc/sys/net/ipv4/tcp_moderate_rcvbuf ] && echo "1" >/proc/sys/net/ipv4/tcp_moderate_rcvbuf                                   # Default tcp_moderate_rcvbuf=1
-    [ -f /proc/sys/net/ipv4/tcp_no_metrics_save ] && echo "1" >/proc/sys/net/ipv4/tcp_no_metrics_save                                   # Default tcp_no_metrics_save=0
-    [ -f /proc/sys/net/ipv4/tcp_syn_retries ] && echo "2" >/proc/sys/net/ipv4/tcp_syn_retries                                           # Default tcp_syn_retries=4
-    [ -f /proc/sys/net/ipv4/tcp_synack_retries ] && echo "2" >/proc/sys/net/ipv4/tcp_synack_retries                                     # Default tcp_synack_retries=3
-    [ -f /proc/sys/net/ipv4/tcp_retries2 ] && echo "3" >/proc/sys/net/ipv4/tcp_retries2                                                 # Default tcp_retries2=8
-    [ -f /proc/sys/net/ipv4/tcp_rmem ] && echo "4096 262144 4194304" >/proc/sys/net/ipv4/tcp_rmem                                       # Default tcp_rmem=524288 1048576 2097152
-    [ -f /proc/sys/net/ipv4/tcp_wmem ] && echo "4096 262144 4194304" >/proc/sys/net/ipv4/tcp_wmem                                       # Default tcp_wmem=262144 212992 8388608
-    [ -f /proc/sys/net/ipv4/udp_rmem_min ] && echo "8192" >/proc/sys/net/ipv4/udp_rmem_min                                              # Default udp_rmem_min=4096
-    [ -f /proc/sys/net/ipv4/udp_wmem_min ] && echo "8192" >/proc/sys/net/ipv4/udp_wmem_min                                              # Default udp_wmem_min=4096
-    [ -f /proc/sys/net/ipv4/tcp_tw_reuse ] && echo "2" >/proc/sys/net/ipv4/tcp_tw_reuse                                                 # Default tcp_tw_reuse=2
-    [ -f /proc/sys/net/ipv4/tcp_max_syn_backlog ] && echo "1024" >/proc/sys/net/ipv4/tcp_max_syn_backlog                                # Default tcp_max_syn_backlog=128
-    [ -f /proc/sys/net/ipv4/tcp_fastopen ] && echo "3" >/proc/sys/net/ipv4/tcp_fastopen                                                 # Default tcp_fastopen=1
-    [ -f /proc/sys/net/ipv4/neigh/default/delay_first_probe_time ] && echo "1" >/proc/sys/net/ipv4/neigh/default/delay_first_probe_time # Default delay_first_probe_time=5
-    [ -f /sys/class/net/wlan0/tx_queue_len ] && echo "1024" >/sys/class/net/wlan0/tx_queue_len                                          # Default tx_queue_len=3000
+    [ -f /proc/sys/net/core/netdev_max_backlog ] && echo "300" >/proc/sys/net/core/netdev_max_backlog                                   # Default netdev_max_backlog=1000, Check via "cat /proc/sys/net/core/netdev_max_backlog"
+    [ -f /proc/sys/net/core/rmem_default ] && echo "262144" >/proc/sys/net/core/rmem_default                                            # Default rmem_default=212992, Check via "cat /proc/sys/net/core/rmem_default"
+    [ -f /proc/sys/net/core/rmem_max ] && echo "4194304" >/proc/sys/net/core/rmem_max                                                   # Default rmem_max=16777216, Check via "cat /proc/sys/net/core/rmem_max"
+    [ -f /proc/sys/net/core/wmem_default ] && echo "262144" >/proc/sys/net/core/wmem_default                                            # Default wmem_default=212992, Check via "cat /proc/sys/net/core/wmem_default"
+    [ -f /proc/sys/net/core/wmem_max ] && echo "4194304" >/proc/sys/net/core/wmem_max                                                   # Default wmem_max=8388608, Check via "cat /proc/sys/net/core/wmem_max"
+    [ -f /proc/sys/net/core/somaxconn ] && echo "1024" >/proc/sys/net/core/somaxconn                                                    # Default somaxconn=128, Check via "cat /proc/sys/net/core/somaxconn"
+    [ -f /proc/sys/net/ipv4/tcp_slow_start_after_idle ] && echo "0" >/proc/sys/net/ipv4/tcp_slow_start_after_idle                       # Default tcp_slow_start_after_idle=1, Check via "cat /proc/sys/net/ipv4/tcp_slow_start_after_idle"
+    [ -f /proc/sys/net/ipv4/tcp_low_latency ] && echo "1" >/proc/sys/net/ipv4/tcp_low_latency                                           # Default tcp_low_latency=0, Check via "cat /proc/sys/net/ipv4/tcp_low_latency"
+    [ -f /proc/sys/net/ipv4/tcp_sack ] && echo "1" >/proc/sys/net/ipv4/tcp_sack                                                         # Default tcp_sack=1, Check via "cat /proc/sys/net/ipv4/tcp_sack"
+    [ -f /proc/sys/net/ipv4/tcp_fack ] && echo "1" >/proc/sys/net/ipv4/tcp_fack                                                         # Default tcp_fack=0, Check via "cat /proc/sys/net/ipv4/tcp_fack"
+    [ -f /proc/sys/net/ipv4/tcp_window_scaling ] && echo "1" >/proc/sys/net/ipv4/tcp_window_scaling                                     # Default tcp_window_scaling=1, Check via "cat /proc/sys/net/ipv4/tcp_window_scaling"
+    [ -f /proc/sys/net/ipv4/tcp_moderate_rcvbuf ] && echo "1" >/proc/sys/net/ipv4/tcp_moderate_rcvbuf                                   # Default tcp_moderate_rcvbuf=1, Check via "cat /proc/sys/net/ipv4/tcp_moderate_rcvbuf"
+    [ -f /proc/sys/net/ipv4/tcp_no_metrics_save ] && echo "1" >/proc/sys/net/ipv4/tcp_no_metrics_save                                   # Default tcp_no_metrics_save=0, Check via "cat /proc/sys/net/ipv4/tcp_no_metrics_save"
+    [ -f /proc/sys/net/ipv4/tcp_syn_retries ] && echo "2" >/proc/sys/net/ipv4/tcp_syn_retries                                           # Default tcp_syn_retries=4, Check via "cat /proc/sys/net/ipv4/tcp_syn_retries"
+    [ -f /proc/sys/net/ipv4/tcp_synack_retries ] && echo "2" >/proc/sys/net/ipv4/tcp_synack_retries                                     # Default tcp_synack_retries=3, Check via "cat /proc/sys/net/ipv4/tcp_synack_retries"
+    [ -f /proc/sys/net/ipv4/tcp_retries2 ] && echo "3" >/proc/sys/net/ipv4/tcp_retries2                                                 # Default tcp_retries2=8, Check via "cat /proc/sys/net/ipv4/tcp_retries2"
+    [ -f /proc/sys/net/ipv4/tcp_rmem ] && echo "4096 262144 4194304" >/proc/sys/net/ipv4/tcp_rmem                                       # Default tcp_rmem=524288 1048576 2097152, Check via "cat /proc/sys/net/ipv4/tcp_rmem"
+    [ -f /proc/sys/net/ipv4/tcp_wmem ] && echo "4096 262144 4194304" >/proc/sys/net/ipv4/tcp_wmem                                       # Default tcp_wmem=262144 212992 8388608, Check via "cat /proc/sys/net/ipv4/tcp_wmem"
+    [ -f /proc/sys/net/ipv4/udp_rmem_min ] && echo "8192" >/proc/sys/net/ipv4/udp_rmem_min                                              # Default udp_rmem_min=4096, Check via "cat /proc/sys/net/ipv4/udp_rmem_min"
+    [ -f /proc/sys/net/ipv4/udp_wmem_min ] && echo "8192" >/proc/sys/net/ipv4/udp_wmem_min                                              # Default udp_wmem_min=4096, Check via "cat /proc/sys/net/ipv4/udp_wmem_min"
+    [ -f /proc/sys/net/ipv4/tcp_tw_reuse ] && echo "2" >/proc/sys/net/ipv4/tcp_tw_reuse                                                 # Default tcp_tw_reuse=2, Check via "cat /proc/sys/net/ipv4/tcp_tw_reuse"
+    [ -f /proc/sys/net/ipv4/tcp_max_syn_backlog ] && echo "1024" >/proc/sys/net/ipv4/tcp_max_syn_backlog                                # Default tcp_max_syn_backlog=128, Check via "cat /proc/sys/net/ipv4/tcp_max_syn_backlog"
+    [ -f /proc/sys/net/ipv4/tcp_fastopen ] && echo "3" >/proc/sys/net/ipv4/tcp_fastopen                                                 # Default tcp_fastopen=1, Check via "cat /proc/sys/net/ipv4/tcp_fastopen"
+    [ -f /proc/sys/net/ipv4/neigh/default/delay_first_probe_time ] && echo "1" >/proc/sys/net/ipv4/neigh/default/delay_first_probe_time # Default delay_first_probe_time=5, Check via "cat /proc/sys/net/ipv4/neigh/default/delay_first_probe_time"
+    [ -x /system/bin/iw ] && /system/bin/iw wlan0 set power_save off                                                                    # Default power_save=on, Check via "/system/bin/iw wlan0 get power_save"
+    [ -f /sys/class/net/wlan0/tx_queue_len ] && echo "1024" >/sys/class/net/wlan0/tx_queue_len                                          # Default tx_queue_len=3000, Check via "cat /sys/class/net/wlan0/tx_queue_len"
 
-    command -v cmd && cmd wifi remove-all-suggestions
+    command -v cmd && cmd wifi remove-all-suggestions                              # Default remove-all-suggestions=none, Check via "cmd wifi list-suggestions"
+    command -v cmd && cmd wifi set-ipreach-disconnect disabled                     # Default set-ipreach-disconnect=true, Check via "cmd wifi get-ipreach-disconnect"
+    command -v cmd && cmd wifi set-network-selection-config disabled disabled -a 0 # Check via "dumpsys wifi | grep -i 'mSufficiencyCheckEnabledWhenScreen'"
+    command -v cmd && cmd wifi set-scan-always-available disabled                  # Risky script (may reduce GPS accuracy), Default set-scan-always-available=null, Check via "settings get global wifi_scan_always_enabled"
 
     [ -f /proc/sys/net/ipv4/tcp_congestion_control ] && echo "bbrplus" >/proc/sys/net/ipv4/tcp_congestion_control
     [ -f /sys/class/net/wlan0/queues/rx-0/rps_cpus ] && echo "ff" >/sys/class/net/wlan0/queues/rx-0/rps_cpus
@@ -49,13 +53,23 @@
     setprop debug.sf.enable_adpf_cpu_hint 1
     [ -f /sys/devices/system/cpu/cpufreq/policy0/scaling_governor ] && echo "performance" >/sys/devices/system/cpu/cpufreq/policy0/scaling_governor
     [ -f /sys/devices/system/cpu/cpufreq/policy4/scaling_governor ] && echo "performance" >/sys/devices/system/cpu/cpufreq/policy4/scaling_governor
-    sleep 5
-    if [ "$(getprop ro.product.device)" = "fog" ]; then
-        if grep -q "VinNet" "/vendor/etc/wifi/WCNSS_qcom_cfg.ini"; then
-            su shell -c "cmd notification post -S bigtext -t 'VinNet' 'MountSuccess' 'Vendor configuration has been successfully mounted'"
-        else
-            su shell -c "cmd notification post -S bigtext -t 'VinNet' 'MountFailed' 'Vendor configuration failed to mount. Install meta module to resolve issue'"
-        fi
+
+    if [ "$(getprop ro.product.device)" != "fog" ]; then
+        VMS="Vendor Mount Status: Failed (Device isn't fog)"
+    elif grep -q "VinNet" "/vendor/etc/wifi/WCNSS_qcom_cfg.ini"; then
+        VMS="Vendor Mount Status: Success"
+    else
+        VMS="Vendor Mount Status: Fail (Require Meta Module)"
     fi
+    if [ -f /system/bin/iw* ] || [ -f /vendor/bin/iw* ] || [ -f /system/xbin/iw* ]; then
+        BMS="Binary Mount Status: Success"
+    else
+        BMS="Binary Mount Status: Fail (Require Meta Module)"
+    fi
+
+    VBMS="${VMS}
+${BMS}"
+
+    su shell -c "cmd notification post -S bigtext -t 'VinNet' 'Mount Status' '$VBMS'"
 
 ) &
