08d6cb2845b415261cfbd553650fd6950a3f027f
Vinzz1234567890 <kevinsusanto602@gmail.com>
2026-08-23 21:58:07 +0700

    Update uninstall script to delete VinNet.log

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,8 @@
 #!/system/bin/sh
 settings delete global wifi_scan_always_enabled
-resetprop -p --delete ro.boot.wificountrycode
+resetprop --delete ro.boot.wificountrycode
 resetprop -p --delete persist.sys.radio.force_lte_ca
 settings delete global network_avoid_bad_wifi
 settings delete global ble_scan_always_enabled
 settings delete global mobile_data_always_on
+rm /storage/emulated/0/Download/VinNet.log
