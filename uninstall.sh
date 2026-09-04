4ebbc3a2b92157c7f3735f087256ae7f771aa5c6
Vinzz <kevinsusanto602@gmail.com>
2026-09-04 22:23:25 +0700

    Add command to delete wifi scan throttle setting

--- uninstall.sh
+++ uninstall.sh
@@ -5,4 +5,5 @@ resetprop -p --delete persist.sys.radio.force_lte_ca
 settings delete global network_avoid_bad_wifi
 settings delete global ble_scan_always_enabled
 settings delete global mobile_data_always_on
+settings delete global wifi_scan_throttle_enabled
 rm /storage/emulated/0/Download/VinNet.log
