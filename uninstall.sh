ac0c83ba98863b24bc05fdd129ca403a770e7119
Vinzz1234567890 <kevinsusanto602@gmail.com>
2026-09-19 21:06:25 +0700

    Remove unnecessary Wi-Fi settings during uninstallation

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 #!/system/bin/sh
+settings delete global wifi_scan_always_enabled
 resetprop --delete ro.boot.wificountrycode
 resetprop -p --delete persist.sys.radio.force_lte_ca
 settings delete global network_avoid_bad_wifi
