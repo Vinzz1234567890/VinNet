823e618a89c11a7b6e92c3a2e93b606e6f63d1d0
Vinzz1234567890 <kevinsusanto602@gmail.com>
2026-09-20 09:43:32 +0700

    Refactor uninstall script: remove unused wifi_scan_always_enabled setting

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/system/bin/sh
-settings delete global wifi_scan_always_enabled
 resetprop --delete ro.boot.wificountrycode
 resetprop -p --delete persist.sys.radio.force_lte_ca
 settings delete global network_avoid_bad_wifi
